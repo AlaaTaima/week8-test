bef66e0a6b15b50d01066cdaf55cb927afb9dbe3
AlaaTaima <alaa.ktaima@gmail.com>
2019-09-07 01:33:24 +0300

    set heroku

--- server/database/config/connection.js
+++ server/database/config/connection.js
@@ -1,21 +1,20 @@
-const { Pool } = require('pg');
+const { Pool } = require("pg");
 
-require('env2')('./config.env');
+require("env2")("./config.env");
 
-let DB_URL = '';
+let DB_URL = "";
 
-if (process.env.NODE_ENV === 'development') {
+if (process.env.NODE_ENV === "development") {
   DB_URL = process.env.DB_URL;
 } else {
   DB_URL = process.env.DATABASE_URL;
 }
-
 
 const options = {
   connectionString: DB_URL,
   ssl: true
 };
 
-if (!DB_URL) throw new Error('Database URL Not Found!!');
+if (!DB_URL) throw new Error("Database URL Not Found!!");
 
 module.exports = new Pool(options);
